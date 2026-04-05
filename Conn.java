b64aeabf6e9e1863e9d307bdb980d116ec20c2aa
Trisha1246 <kolla.Trisha2006@gmail.com>
2026-04-05 14:39:57 +0530

    Refactor Conn class for ATM simulator database
    Updated the Conn class to connect to the ATM simulator database instead of the bank management system.

--- Conn.java
+++ Conn.java
@@ -1,17 +1,20 @@
-package bank.management.system;
+package atm.simulator.system;
 
 import java.sql.*;
 
 public class Conn {
+    
     Connection c;
     Statement s;
     public Conn() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","Trisha@2006");
-            s = c.createStatement();
-        } catch(Exception e) {
-            System.out.println(e);
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/atmsimulatorsystem"
+              , "root", "Trisha@2006");
+            s=c.createStatement();
+            
+        } catch (Exception e){
+           e.printStackTrace();
         }
     }
+    
 }
